6b1453e1756c85054823e9b1e5a525e82a1257b6
krimongurung524-rgb <krimongurung524@gmail.com>
2026-07-15 23:39:41 +0545

    scratch .py is added

---
+++ basic/scratch.py
@@ -0,0 +1,34 @@
+# List
+fruits = ["Apple", "Banana", "Mango"]
+# (append) method le last ma items thapne kam garcha
+fruits.append("Grapes") 
+
+# (insert) le chei hamilai man lageko thau ma items add garna sakchum
+fruits.insert(2, "Kiwi")
+
+# (extend) le chei dherai items haru ekaichoti add garna milcha
+fruits.extend(["Pineapple","Dragon fruit", "Litchi"])
+
+# (remove) le chei hamilai man lageko item remove garna sakchum
+fruits.remove("Mango")
+
+# (pop) le chei indexing bata jun items hatauna man lagcha tesle hataidincha yedi empty rakhe ma end ko item hataidincha
+fruits.pop(1)
+fruits.pop()
+
+# (del) le chei index or purai list lai hataune kam garcha
+del fruits [1]
+
+# clear
+fruits.clear
+print(fruits)
+
+
+# (sort) le chei ascending milaune kam garcha 
+# reverse le chei ulto bata print garne kam garcha
+numbers = [5,3,4,2,8,9,6]
+numbers.sort()
+numbers.sort(reverse = True)
+print(numbers)
+print(len(numbers))
+
