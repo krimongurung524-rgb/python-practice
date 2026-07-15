5c5391d391a16c963154ba2c68d044d5d5bc1eb4
krimongurung524-rgb <krimongurung524@gmail.com>
2026-07-15 22:34:46 +0545

    Added git.py for testing

---
+++ basic/git.py
@@ -0,0 +1,8 @@
+class football:
+    def __init__(self, stadium, players, rule):
+        self.stadium = stadium
+        self.players = players
+        self.rule = rule
+               
+s1 = football("lusail", "11", "offside")
+print(s1.stadium)
