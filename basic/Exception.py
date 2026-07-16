f9f96948fb2ac04ce982eaf7419c1182ab0092db
Krimon Gurung <krimongurung524@gmail.com>
2026-07-16 23:58:40 +0545

    Little code is added

--- basic/Exception.py
+++ basic/Exception.py
@@ -1,11 +1,11 @@
-# try:
-#     result = 10/0
+try:
+    result = 10/0
     
-# except:
-#     print("Error! Zero can't be divided")
-#     print("Program is running")
+except:
+    print("Error! Zero can't be divided")
+    print("Program is running")
     
-
+# Question 2
 try:
     num = int(input("Enter the number: "))
     result = 10/num
@@ -16,3 +16,19 @@ except ZeroDivisionError:
     
 except ValueError:
     print("Enter the number! not text")
+    
+# Question 3
+try:
+    result = 10/0
+
+except ZeroDivisionError as e:
+    print("Error :" , e)
+    
+# Question 4
+try:
+    result = 10/0
+except ZeroDivisionError:
+    print("Error")
+    
+else:
+    print("Everything good, result: ", result)
